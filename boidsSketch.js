0499f4e359f37c425118f007123848dbb0d18b56
Abrar Akhand <Jahin345@gmail.com>
2021-06-08 19:09:35 -0700

    Smooth appear/disappear transition for nav buttons

--- boidsSketch.js
+++ boidsSketch.js
@@ -59,6 +59,7 @@ function Flock() {
 }
 
 Flock.prototype.run = function () {
+    // TODO: Fix this (Very Bad Performance O(n^2))
     for (let i = 0; i < this.boids.length; i++) {
         this.boids[i].run(this.boids);  // Passing the entire list of boids to each boid individually
     }
--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-// $(window).scroll(function () {
-//     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-//         alert("bottom!");
-//     }
-// });
 
+var skillsScrollPos, projectsScrollPos;
 
+// Will hold half the size of a section
+// Used to detect when user is halfway on a page
+var halfSectionPos;
+
+// update scroll position for the sections when window size changes
+$(window).resize(function () {
+
+    skillsScrollPos = $(".hero").height();
+
+    projectsScrollPos = $('.skills-section').height() + skillsScrollPos;
+    console.log(`Window resized, skillsScrollPos: ${skillsScrollPos} projectsScrollPos: ${projectsScrollPos}`);
+
+    halfSectionPos = skillsScrollPos / 2;
+});
+
+/* The following is only run when the document has fully loaded */
 $(document).ready(function () {
     console.log("ready!");
     skillsScrollPos = $(".hero").height();
@@ -12,13 +24,18 @@ $(document).ready(function () {
     projectsScrollPos = $('.skills-section').height() + skillsScrollPos;
     console.log(projectsScrollPos);
 
-    // Figure out the current page.
+    halfSectionPos = skillsScrollPos / 2;
+
+    // Figure out the initial current page when page is loaded.
     currScrollAmount = $(window).scrollTop();
     currPage =
         (currScrollAmount < skillsScrollPos)
             ? 1 :
             (currScrollAmount >= skillsScrollPos && currScrollAmount < projectsScrollPos
                 ? 2 : 3);
+
+    // Initialize buttons visibility
+    updateNavButtons();
 
     //projectsScrollPos = $('.projects-section').height() + skillsScrollPos;
     //console.log(projectsScrollPos);
@@ -43,6 +60,22 @@ $(document).ready(function () {
 
     });
 
+    // For top nav buttons
+    $(".top-nav-btn").click(function () {
+        console.log('btn pushed');
+        locationToScroll = $(this).find("a").attr("href");
+
+        // Using jQuery's animate() method to add smooth page scroll since smooth scroll is not supported in safari
+
+        $('html, body').animate({
+            scrollTop: $(locationToScroll).offset().top
+        }, 800);
+
+        return false;
+
+    });
+
+    var onNewSection = false;
 
     // Determines the current location of the user utilizing the the page scroll position.  
     $(window).scroll(function () {
@@ -51,26 +84,89 @@ $(document).ready(function () {
         console.log(`current scroll= ${currScrollAmount}, skills pos= ${skillsScrollPos}`);
 
         // TODO: Make it so the buttons disappear and reappear when on certain part of the page.
-        if (currScrollAmount < skillsScrollPos && currPage != 1) {
+        if (currScrollAmount < skillsScrollPos / 2 && currPage != 1) {
 
-            alert('first page');
             currPage = 1;
+            onNewSection = true;
 
         }
-        else if (currScrollAmount >= skillsScrollPos && currScrollAmount < projectsScrollPos && currPage != 2) {
+        else if (currScrollAmount >= skillsScrollPos / 2 && currScrollAmount < projectsScrollPos - halfSectionPos && currPage != 2) {
 
-            alert('second page');
             currPage = 2;
+            onNewSection = true;
 
-        } else if (currScrollAmount >= projectsScrollPos && currPage != 3) {
+        } else if (currScrollAmount >= projectsScrollPos - halfSectionPos && currPage != 3) {
 
-            alert('third page')
             currPage = 3;
+            onNewSection = true;
+        }
+
+
+        if (onNewSection) {
+            onNewSection = false;
+
+            updateNavButtons();
+
         }
 
     });
 });
 
+// Change visibility of the buttons based on page the user is on
+function updateNavButtons() {
+
+    switch (currPage) {
+        case 1:
+            /*
+            $('#top-nav-btn1').css('visibility', 'hidden');
+            $('#top-nav-btn2').css('visibility', 'hidden');
+
+            $('#bottom-nav-btn1').css('visibility', 'visible');
+            $('#bottom-nav-btn2').css('visibility', 'visible');
+            
+            */
+
+            $('#top-nav-btn1').addClass('top-nav-hidden');
+            $('#top-nav-btn2').addClass('top-nav-hidden');;
+
+            $('#bottom-nav-btn1').removeClass('bottom-nav-hidden');
+            $('#bottom-nav-btn2').removeClass('bottom-nav-hidden');
+
+            break;
+        case 2:
+            /*
+            $('#top-nav-btn1').css('visibility', 'visible');
+            $('#top-nav-btn2').css('visibility', 'hidden');
+
+            $('#bottom-nav-btn1').css('visibility', 'hidden');
+            $('#bottom-nav-btn2').css('visibility', 'visible');
+            */
+            $('#top-nav-btn1').removeClass('top-nav-hidden');
+            $('#top-nav-btn2').addClass('top-nav-hidden');;
+
+            $('#bottom-nav-btn1').addClass('bottom-nav-hidden');
+            $('#bottom-nav-btn2').removeClass('bottom-nav-hidden');
+
+            break;
+        case 3:
+            /*
+            $('#top-nav-btn1').css('visibility', 'visible');
+            $('#top-nav-btn2').css('visibility', 'visible');
+
+            $('#bottom-nav-btn1').css('visibility', 'hidden');
+            $('#bottom-nav-btn2').css('visibility', 'hidden');
+            */
+            $('#top-nav-btn1').removeClass('top-nav-hidden');
+            $('#top-nav-btn2').removeClass('top-nav-hidden');;
+
+            $('#bottom-nav-btn1').addClass('bottom-nav-hidden');
+            $('#bottom-nav-btn2').addClass('bottom-nav-hidden');
+
+            break;
+        default:
+            console.warn('default use case hit in switch stmt');
+    }
+}
 
 
 
